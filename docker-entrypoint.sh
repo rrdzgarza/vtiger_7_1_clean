223c5689c1b11c99d4c275a298d787d6ba9eeca2
RICARDO RODRIGUEZ GARZA <rrdzgarza@gmail.com>
2026-01-10 10:06:39 -0600

    corrige parametros de base de datos

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,7 +14,25 @@ if [ ! -s /var/www/html/config.inc.php ]; then
     fi
 fi
 
-# Apply Envs if config exists and vars are set
+fi
+
+# Replace Template Placeholders if they exist (handling config from template)
+if [ -f /var/www/html/config.inc.php ]; then
+    # Replace Database Placeholders
+    # usage of | delimiter to handle special chars in passwords
+    if [ ! -z "$DB_HOSTNAME" ]; then sed -i "s|_DBC_SERVER_|${DB_HOSTNAME}|g" /var/www/html/config.inc.php; fi
+    if [ ! -z "$DB_USERNAME" ]; then sed -i "s|_DBC_USER_|${DB_USERNAME}|g" /var/www/html/config.inc.php; fi
+    if [ ! -z "$DB_PASSWORD" ]; then sed -i "s|_DBC_PASS_|${DB_PASSWORD}|g" /var/www/html/config.inc.php; fi
+    if [ ! -z "$DB_NAME" ]; then sed -i "s|_DBC_NAME_|${DB_NAME}|g" /var/www/html/config.inc.php; fi
+    
+    # Set DB Type to mysqli
+    sed -i "s|_DBC_TYPE_|mysqli|g" /var/www/html/config.inc.php
+    # Set Port (remove colon if present in template)
+    sed -i "s|:_DBC_PORT_|3306|g" /var/www/html/config.inc.php
+    sed -i "s|_DBC_PORT_|3306|g" /var/www/html/config.inc.php
+fi
+
+# Apply Envs if config exists and vars are set (Legacy variable names)
 if [ -f /var/www/html/config.inc.php ]; then
     echo "Checking for environment variables to configure Vtiger..."
     
